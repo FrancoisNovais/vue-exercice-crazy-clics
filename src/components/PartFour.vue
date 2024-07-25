33acee19169d5c99898015c2aeb0cc034b8b79d9
francois6381 <francois.novais@gmail.com>
2024-07-25 23:26:02 +0200

    PartFour composant structure

--- src/components/PartFour.vue
+++ src/components/PartFour.vue
@@ -1,9 +1,28 @@
-<script setup></script>
+<script setup>
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max)
+}
+
+const displayRandomNumber = () => {
+  alert(getRandomInt(99))
+}
+</script>
 
 <template>
   <div>
-    <p>Part four</p>
+    <button @:click="displayRandomNumber">Chiffre surprise</button>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+button {
+  background-color: #fc7440;
+  border: none;
+  border-style: double;
+  color: white;
+  padding: 65px 20px;
+  text-align: center;
+  font-size: 16px;
+  border-radius: 50%;
+}
+</style>
