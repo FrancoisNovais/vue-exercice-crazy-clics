417e21746ba9d2ebbfaf42ada12a2cddef543125
francois6381 <francois.novais@gmail.com>
2024-07-26 02:04:51 +0200

    Bonus1

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import PartThree from './components/PartThree.vue'
 import PartFour from './components/PartFour.vue'
 import PartFive from './components/PartFive.vue'
 import PartSix from './components/PartSix.vue'
+import Bonus1 from './components/Bonus1.vue'
 </script>
 <template>
   <main>
@@ -15,6 +16,7 @@ import PartSix from './components/PartSix.vue'
     <PartFour />
     <PartFive />
     <PartSix />
+    <Bonus1 />
   </main>
 </template>
 
---
+++ src/components/Bonus1.vue
@@ -0,0 +1,39 @@
+<script setup>
+const word1 = 'aLigaTOR'
+const word2 = 'CHAMOIS'
+const word3 = 'ElEpHaNt'
+const word4 = 'wombaT'
+
+const correctTheWord = (word) => {
+  const firstLetter = word.charAt(0)
+
+  const firstLetterCap = firstLetter.toUpperCase()
+
+  const remainingLetters = word.slice(1).toLowerCase()
+
+  const capitalizedWord = firstLetterCap + remainingLetters
+  alert(capitalizedWord)
+}
+</script>
+
+<template>
+  <section>
+    <button @:click="correctTheWord(word1)">{{ word1 }}</button>
+    <button @:click="correctTheWord(word2)">{{ word2 }}</button>
+    <button @:click="correctTheWord(word3)">{{ word3 }}</button>
+    <button @:click="correctTheWord(word4)">{{ word4 }}</button>
+  </section>
+</template>
+
+<style scoped>
+section {
+  justify-content: center;
+  gap: 30px;
+}
+button {
+  background-color: #62e5b3;
+  padding: 7px 15px;
+  border: none;
+  border-radius: 5px;
+}
+</style>
